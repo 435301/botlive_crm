1cadcf3553307ed025a0a92ee3e628184c2952f0
RICH LABZ <padmavathi.richlabz@gmail.com>
2026-03-07 11:58:05 +0530

    pagination to max 4 pages

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  const pages = [...Array(totalPages).keys()].map((n) => n + 1);
+  // const pages = [...Array(totalPages).keys()].map((n) => n + 1);
+  const maxPagesToShow = 4;
 
+  let startPage = Math.max(currentPage - 1, 1);
+  let endPage = startPage + maxPagesToShow - 1;
+
+  if (endPage > totalPages) {
+    endPage = totalPages;
+    startPage = Math.max(endPage - maxPagesToShow + 1, 1);
+  }
+
+  const pages = [];
+  for (let i = startPage; i <= endPage; i++) {
+    pages.push(i);
+  }
   return (
     <div className="pagination-container">
       <button
@@ -16,9 +29,8 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       {pages.map((page) => (
         <button
           key={page}
-          className={`page-btn ${
-            currentPage === page ? "active" : ""
-          }`}
+          className={`page-btn ${currentPage === page ? "active" : ""
+            }`}
           onClick={() => onPageChange(page)}
         >
           {page}
